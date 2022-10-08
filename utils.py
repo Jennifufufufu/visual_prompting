f963501576386050b9ed32c78aab0d74c681e1c7
Jennifufufufu <84007825+Jennifufufufu@users.noreply.github.com>
2022-10-08 19:43:04 +1100

    Update utils.py

--- utils.py
+++ utils.py
@@ -97,21 +97,25 @@ def cosine_lr(optimizer, base_lr, warmup_length, steps):
     return _lr_adjuster
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        import pdb
+        pdb.set_trace()
+        maxk = max(topk)
+        batch_size = target.size(0)
 
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
+        _, pred = output.topk(maxk, 1, True, True)
+        _, label = target.topk(maxk, 1, True, True)
+        pred = pred.t()
+        label = label.t()
+        correct = pred.eq(label.expand_as(pred))
 
-        res = []
-        for k in topk:
-            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
 
 
 class AverageMeter(object):
